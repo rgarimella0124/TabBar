a9eb773a90b5a686e06a8dae4e8276bb7578b121
Ram Simran <ram@payzello.in>
2019-12-24 18:28:56 +0530

    36 Updated

--- src/navigation/BaseNavigator.js
+++ src/navigation/BaseNavigator.js
@@ -1,134 +1,106 @@
-import React from 'react';
-import {createAppContainer, createBottomTabNavigator, createStackNavigator} from 'react-navigation';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import {MultiBar, MultiBarToggle} from 'react-native-multibar';
+import React from "react";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 
-import {Bookmarks, Likes, Private, Profile, Settings} from "../components";
-import {Routes} from "./Routes";
+import { createStackNavigator } from "react-navigation-stack";
 
-const TabsNavigator = createBottomTabNavigator({
+import { createAppContainer } from "react-navigation";
+import { FontAwesome } from "@expo/vector-icons";
+import { MultiBar, MultiBarToggle } from "react-native-multibar";
+
+import { Bookmarks, Likes, Private, Profile, Settings } from "../components";
+import { Routes } from "./Routes";
+
+const TabsNavigator = createBottomTabNavigator(
+  {
     [Routes.TabsBookmarks]: {
-        screen: Bookmarks,
-        navigationOptions: () => ({
-            tabBarIcon: ({tintColor}) => (
-                <Icon
-                    name="bookmark"
-                    color={tintColor}
-                    size={24}
-                />
-            )
-        })
+      screen: Bookmarks,
+      navigationOptions: () => ({
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesome name="bookmark" color={tintColor} size={24} />
+        )
+      })
     },
     [Routes.TabsLikes]: {
-        screen: Likes,
-        navigationOptions: () => ({
-            tabBarIcon: ({tintColor}) => (
-                <Icon
-                    name="heart"
-                    color={tintColor}
-                    size={24}
-                />
-            )
-        })
+      screen: Likes,
+      navigationOptions: () => ({
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesome name="heart" color={tintColor} size={24} />
+        )
+      })
     },
     MultiBar: {
-        screen: () => null,
-        navigationOptions: ({navigation}) => ({
-            tabBarIcon: () => (
-                <MultiBarToggle
-                    navigation={navigation}
-                    actionSize={30}
-                    routes={[
-                        {
-                            routeName: Routes.OtherScreen,
-                            color: '#FF8360',
-                            icon: (
-                                <Icon
-                                    name="rocket"
-                                    color="#333333"
-                                    size={15}
-                                />
-                            )
-                        },
-                        {
-                            routeName: Routes.OtherScreen,
-                            color: '#E8E288',
-                            icon: (
-                                <Icon
-                                    name="dashboard"
-                                    color="#333333"
-                                    size={15}
-                                />
-                            )
-                        },
-                        {
-                            routeName: Routes.OtherScreen,
-                            color: '#7DCE82',
-                            icon: (
-                                <Icon
-                                    name="gears"
-                                    color="#333333"
-                                    size={15}
-                                />
-                            )
-                        },
-                    ]}
-                    icon={(
-                        <Icon
-                            name="plus"
-                            color="#FFFFFF"
-                            size={24}
-                        />
-                    )}
-                />
-            )
-        }),
-        params: {
-            navigationDisabled: true
-        }
+      screen: () => null,
+      navigationOptions: ({ navigation }) => ({
+        tabBarIcon: () => (
+          <MultiBarToggle
+            navigation={navigation}
+            actionSize={30}
+            routes={[
+              {
+                routeName: Routes.OtherScreen,
+                color: "#FF8360",
+                icon: <FontAwesome name="rocket" color="#333333" size={15} />
+              },
+              {
+                routeName: Routes.OtherScreen,
+                color: "#E8E288",
+                icon: <FontAwesome name="dashboard" color="#333333" size={15} />
+              },
+              {
+                routeName: Routes.OtherScreen,
+                color: "#7DCE82",
+                icon: <FontAwesome name="gears" color="#333333" size={15} />
+              }
+            ]}
+            icon={<FontAwesome name="plus" color="#FFFFFF" size={24} />}
+          />
+        )
+      }),
+      params: {
+        navigationDisabled: true
+      }
     },
     [Routes.TabsPrivate]: {
-        screen: Private,
-        navigationOptions: () => ({
-            tabBarIcon: ({tintColor}) => (
-                <Icon
-                    name="lock"
-                    color={tintColor}
-                    size={24}
-                />
-            )
-        })
+      screen: Private,
+      navigationOptions: () => ({
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesome name="lock" color={tintColor} size={24} />
+        )
+      })
     },
     [Routes.TabsProfile]: {
-        screen: Profile,
-        navigationOptions: () => ({
-            tabBarIcon: ({tintColor}) => (
-                <Icon
-                    name="user"
-                    color={tintColor}
-                    size={24}
-                />
-            )
-        })
+      screen: Profile,
+      navigationOptions: () => ({
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesome name="user" color={tintColor} size={24} />
+        )
+      })
     }
-}, {
+  },
+  {
     tabBarComponent: MultiBar,
     tabBarOptions: {
-        showLabel: false,
-        activeTintColor: '#F8F8F8',
-        inactiveTintColor: '#586589',
-        style: {
-            backgroundColor: '#171F33'
-        },
-        tabStyle: {}
+      showLabel: false,
+      activeTintColor: "#F8F8F8",
+      inactiveTintColor: "#586589",
+      style: {
+        backgroundColor: "#171F33"
+      },
+      tabStyle: {}
     }
-});
+  }
+);
 
-const BaseNavigatorContainer = createAppContainer(createStackNavigator({
-    [Routes.Tabs]: TabsNavigator,
-    [Routes.OtherScreen]: Settings
-}, {
-    headerMode: 'none'
-}));
+const BaseNavigatorContainer = createAppContainer(
+  createStackNavigator(
+    {
+      [Routes.Tabs]: TabsNavigator,
+      [Routes.OtherScreen]: Settings
+    },
+    {
+      headerMode: "none"
+    }
+  )
+);
 
-export {BaseNavigatorContainer as BaseNavigator};
+export { BaseNavigatorContainer as BaseNavigator };
